329d87f7ae366340b659266287bae83e980a6b2a
Aung Myat Kyaw <aungmyatkyaw@Aungs-MacBook-Pro.local>
2023-09-22 11:18:19 +0630

    add close side bar on clicking outside the sidebar

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,21 +1,42 @@
 import './Sidebar.css';
 import Category from './Category/Category';
 import Hamburger from 'hamburger-react'
-import { useState } from 'react';
+import { useState, useEffect, useRef  } from 'react';
 
 
 function Sidebar({handleQuery, categoryData ,handleCategoryQuery}) {
-  const [sidebarIsShown , setSidebarIsShown] = useState(false)
-
+  const [sidebarIsShown , setSidebarIsShown] = useState(false);
+  const [isOpen, setOpen] = useState(false);
+  const sidebarRef = useRef(null);
 
   function toggleSidebar(){
     setSidebarIsShown(!sidebarIsShown)
   }
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        // Clicked outside of the sidebar, so close it
+        toggleSidebar();
+        setOpen(!isOpen)
+      }
+    }
+
+    if (sidebarIsShown) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [sidebarIsShown]);
+
+
   return (
       <>
-        <button className={sidebarIsShown?'sidebar-toggle show': 'sidebar-toggle'} onClick={toggleSidebar}><Hamburger size={20}/></button>
-        <section className={sidebarIsShown?'sidebar show': 'sidebar'}>  
+        <section className={sidebarIsShown?'sidebar show': 'sidebar'} ref={sidebarRef}>  
+        <button className='sidebar-toggle' onClick={toggleSidebar}><Hamburger toggled={isOpen} toggle={setOpen} size={20}/></button>
           <div className='sidebar-container'>
             <input type="text" placeholder='Search your item here' onChange={handleQuery} />
             <Category categoryData={categoryData}  handleQuery={handleCategoryQuery}/>
